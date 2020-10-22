3905eed138807866ba9e64ceac5eedadad089b21
Yehuda Bortz <yehudabortz@icloud.com>
2020-10-22 17:34:33 -0500

    commit tree ef66315fb523b8a3a9618fe01377d373eab641b7

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while 1 <= number
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+    while 1 <= number
+        puts "#{number} SECOND(S)!"
+        sleep 1
+        number -= 1
+    
+    end
+    "HAPPY NEW YEAR!"
+end
+
